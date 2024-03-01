707448bf5bf46dac034c861d32dfc88cb9433764
Connor Roberts <therealconnorroberts@gmail.com>
2024-02-29 17:49:54 -0800

    Dash Not Additive Bug Fixed

--- Assets/Scripts/Actors/Player/PlayerStates/Dash.cs
+++ Assets/Scripts/Actors/Player/PlayerStates/Dash.cs
@@ -8,7 +8,7 @@ public class Dash : StateComponent
     public override void Enter(string msg = "")
     {
         Player.CurrentDashCount--;
-        Player.Velocity = (Player.Velocity.magnitude + PlayerData.DashVelocity) * (Player.CameraRotation.transform.rotation * Vector3.forward) * Time.fixedDeltaTime;
+        Player.Velocity += (Player.Velocity.magnitude + PlayerData.DashVelocity) * (Player.CameraRotation.transform.rotation * Vector3.forward) * Time.fixedDeltaTime;
         Player.Velocity.y += PlayerData.UpwardsDashVelocity * Time.fixedDeltaTime;
         Player.Controller.Move(Player.Velocity);
         StateMachine.TransitionTo("Air");
