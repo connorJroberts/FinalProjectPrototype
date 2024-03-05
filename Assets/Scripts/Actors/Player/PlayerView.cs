45cc451171fdf011a2996c54dc093917c8aeaed2
Connor Roberts <therealconnorroberts@gmail.com>
2024-03-05 12:47:45 -0800

    PlayerView Script Created

---
+++ Assets/Scripts/Actors/Player/PlayerView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerView : NetworkBehaviour
+{
+
+    [SerializeField] private PlayerController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
