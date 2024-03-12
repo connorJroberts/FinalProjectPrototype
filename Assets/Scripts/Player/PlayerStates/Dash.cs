131db7783a90502488afe3da71770950cc282197
Connor Roberts <therealconnorroberts@gmail.com>
2024-03-12 11:18:21 -0700

    Dash Linearized

--- Assets/Scripts/Player/PlayerStates/Dash.cs
+++ Assets/Scripts/Player/PlayerStates/Dash.cs
@@ -8,7 +8,7 @@ public class Dash : State
     public override void Enter(string msg = "")
     {
         Player.CurrentDashCount--;
-        Player.Velocity += (Player.Velocity.magnitude + PlayerData.DashVelocity) * (Player.CameraRotation.transform.rotation * Vector3.forward) * Time.fixedDeltaTime * Player.DashSpeedMultiplier;
+        Player.Velocity += (Player.Velocity.magnitude + PlayerData.DashVelocity) * (Player.CameraRotation.transform.rotation * Vector3.forward) * Time.fixedDeltaTime;
         Player.Velocity.y += PlayerData.UpwardsDashVelocity * Time.fixedDeltaTime;
         Player.Controller.Move(Player.Velocity);
         StateMachine.TransitionTo(new Air());
