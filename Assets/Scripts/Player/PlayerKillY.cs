c1101a6aafc4b7eb5ada36a226222d926765d702
Trex <trexbhaii@gmail.com>
2024-03-14 13:57:11 -0700

    Revert "Merge branch 'develop' of https://github.com/connorJroberts/FinalProjectPrototype into develop"
    This reverts commit a48cc81eea3d3b9a92023fda590f2f655efc0e44, reversing
    changes made to e7672d9cca3d73892b3ef5b6b4d63f8e1a1eeb38.

--- Assets/Scripts/Player/PlayerKillY.cs
+++ Assets/Scripts/Player/PlayerKillY.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 
 public class PlayerKillY : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject SpawnPoint;
-
-    [SerializeField]
-    private float killZ = -1.0f;
-
-    private Vector3 _resetPoint = new Vector3();
-
-    void Awake()
-    {
-
-        _resetPoint = gameObject.transform.position;
-        if (SpawnPoint != null)
-        {
-            _resetPoint = SpawnPoint.transform.position;
-        }
-    }
+    [SerializeField] private float _killYHeight;
+    [SerializeField] private Vector3 _spawn;
 
     // Update is called once per frame
     void Update()
     {
-
-        if (transform.position.y > killZ)
-            return;
-
-        // reset
-        Debug.Log($"Time to reset to {_resetPoint}");
-        transform.position = _resetPoint;
+        if (gameObject.transform.position.y < _killYHeight)
+        {
+            gameObject.transform.position = _spawn;
+        }
     }
 }
